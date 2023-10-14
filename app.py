09c03c663458f2179400ceb4888d0f456928072c
udaykiran1612 <98377158+udaykiran1612@users.noreply.github.com>
2023-10-14 14:42:30 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.datasets import load_heart
+from sklearn.datasets import load_heartdisease
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score
