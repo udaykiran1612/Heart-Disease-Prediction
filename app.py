684390d23ab5057e0d23b5b68817def4cbd26b87
udaykiran1612 <98377158+udaykiran1612@users.noreply.github.com>
2023-10-14 17:14:11 +0530

    app.py

--- app.py
+++ app.py
@@ -1,75 +1,51 @@
 import streamlit as st
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import accuracy_score
+import numpy as np
 
-# Create a Streamlit web app
-st.title("Heart Disease Prediction with Linear Regression")
+# Function for Linear Regression
+def linear_regression_predict(coefficients, feature_vector):
+    """
+    Perform linear regression prediction based on the coefficients and feature vector.
 
-# Load the dataset
-@st.cache
-def load_data():
-    data = pd.read_csv("heart_disease_data.csv")
-    return data
+    Parameters:
+        coefficients (list or numpy.array): Coefficients of the linear regression model.
+        feature_vector (list or numpy.array): Input features for prediction.
 
-data = load_data()
+    Returns:
+        float: Predicted value.
+    """
+    return np.dot(coefficients, feature_vector)
 
-# Data preprocessing
-X = data.drop("target", axis=1)
-y = data["target"]
+# Streamlit interface
+st.title("Heart Disease Prediction")
+st.write("Enter the following information to predict heart disease:")
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Create input fields for user to enter data
+age = st.number_input("Age", min_value=1, max_value=100)
+sex = st.selectbox("Sex", ['Male', 'Female'])
+cp = st.selectbox("Chest Pain Type", [0, 1, 2, 3])
+trestbps = st.number_input("Resting Blood Pressure (mm Hg)", min_value=50, max_value=200)
+chol = st.number_input("Cholesterol (mg/dl)", min_value=50, max_value=400)
+fbs = st.selectbox("Fasting Blood Sugar", [0, 1])
+restecg = st.selectbox("Resting Electrocardiographic Results", [0, 1, 2])
+thalach = st.number_input("Maximum Heart Rate Achieved (bpm)", min_value=70, max_value=200)
+exang = st.selectbox("Exercise-Induced Angina", [0, 1])
+oldpeak = st.number_input("ST Depression Induced by Exercise", min_value=0.0, max_value=6.2)
+slope = st.selectbox("Slope of Peak Exercise ST Segment", [0, 1, 2])
+ca = st.number_input("Number of Major Vessels Colored by Fluoroscopy", min_value=0, max_value=4)
+thal = st.selectbox("Thalassemia Type", [0, 1, 2, 3])
 
-# Create and train a Linear Regression model
-model = LinearRegression()
-model.fit(X_train, y_train)
+# Define example coefficients for linear regression - Replace with your actual model coefficients
+coefficients = np.array([0.1, 0.2, 0.3, -0.1, 0.05, -0.2, 0.1, -0.15, -0.05, 0.2, -0.1, 0.05, -0.2])
 
-# User input for feature values
-st.sidebar.header("User Input:")
-age = st.sidebar.slider("Age:", min_value=29, max_value=77, value=45)
-sex = st.sidebar.selectbox("Sex:", ["Male", "Female"])
-cp = st.sidebar.slider("Chest Pain Type:", min_value=0, max_value=3, value=0)
-trestbps = st.sidebar.slider("Resting Blood Pressure:", min_value=94, max_value=200, value=130)
-chol = st.sidebar.slider("Cholesterol Level:", min_value=126, max_value=564, value=240)
-fbs = st.sidebar.selectbox("Fasting Blood Sugar:", [0, 1])
-restecg = st.sidebar.selectbox("Resting ECG:", [0, 1, 2])
-thalach = st.sidebar.slider("Maximum Heart Rate:", min_value=71, max_value=202, value=160)
-exang = st.sidebar.selectbox("Exercise-Induced Angina:", [0, 1])
-oldpeak = st.sidebar.slider("ST Depression:", min_value=0.0, max_value=6.2, value=1.0)
-slope = st.sidebar.slider("Slope of ST Segment:", min_value=0, max_value=2, value=1)
-ca = st.sidebar.slider("Number of Major Vessels:", min_value=0, max_value=3, value=0)
-thal = st.sidebar.slider("Thalassemia Type:", min_value=0, max_value=3, value=2)
+# Create a button to make predictions
+if st.button("Predict"):
+    # Prepare user input as a feature vector
+    feature_vector = [age, 1 if sex == 'Male' else 0, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
+    
+    # Calculate the prediction using the linear_regression_predict function
+    prediction = linear_regression_predict(coefficients, feature_vector)
 
-# Transform user input into a DataFrame for prediction
-input_data = pd.DataFrame({
-    'age': [age],
-    'sex': [sex],
-    'cp': [cp],
-    'trestbps': [trestbps],
-    'chol': [chol],
-    'fbs': [fbs],
-    'restecg': [restecg],
-    'thalach': [thalach],
-    'exang': [exang],
-    'oldpeak': [oldpeak],
-    'slope': [slope],
-    'ca': [ca],
-    'thal': [thal]
-})
+    st.write(f"Predicted Heart Disease Probability: {prediction:.2f}")
 
-# Make a prediction
-prediction = model.predict(input_data)
+# Note: You should replace the 'coefficients' array with the actual coefficients from your linear regression model.
 
-# Convert the prediction to a binary result
-prediction_result = "Heart Disease" if prediction[0] > 0.5 else "No Heart Disease"
-
-# Calculate the accuracy score
-y_pred = model.predict(X_test)
-y_pred_binary = [1 if pred >= 0.5 else 0 for pred in y_pred]
-accuracy = accuracy_score(y_test, y_pred_binary)
-
-st.header("Prediction:")
-st.write(f"The model predicts that the patient has: {prediction_result}")
-st.write("Accuracy Score:", accuracy)
